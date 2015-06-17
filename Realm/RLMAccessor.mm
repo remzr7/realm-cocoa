21d76643d451bda1becbbea68bd26d3cf4a7b104
Thomas Goyne <tg@realm.io>
2015-06-17 12:39:34 -0700

    Fix observing List<> properties on standalone objects

--- Realm/RLMAccessor.mm
+++ Realm/RLMAccessor.mm
@@ -469,7 +469,7 @@ static IMP RLMAccessorStandaloneGetter(RLMProperty *prop, RLMAccessorCode access
         return imp_implementationWithBlock(^(RLMObjectBase *obj) {
             id val = RLMSuperGet(obj, propName);
             if (!val) {
-                val = [[RLMArray alloc] initWithObjectClassName:objectClassName parentObject:obj key:propName];
+                val = [[RLMArray alloc] initWithObjectClassName:objectClassName];
                 RLMSuperSet(obj, propName, val);
             }
             return val;
@@ -484,7 +484,7 @@ static IMP RLMAccessorStandaloneSetter(RLMProperty *prop, RLMAccessorCode access
         NSString *objectClassName = prop.objectClassName;
         return imp_implementationWithBlock(^(RLMObjectBase *obj, id<NSFastEnumeration> ar) {
             // make copy when setting (as is the case for all other variants)
-            RLMArray *standaloneAr = [[RLMArray alloc] initWithObjectClassName:objectClassName parentObject:obj key:propName];
+            RLMArray *standaloneAr = [[RLMArray alloc] initWithObjectClassName:objectClassName];
             if ((id)ar != NSNull.null) {
                 [standaloneAr addObjects:ar];
             }
