23e6b93b877ec85b7db2220e1ec6d2c2dda65185
Ari Lazier <alazier@gmail.com>
2015-06-22 14:05:35 -0700

    don't create accessor for dynamic realms

--- Realm/RLMRealm.mm
+++ Realm/RLMRealm.mm
@@ -384,10 +384,15 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema, 
 
     __weak RLMRealm *weakRealm = realm;
     config.migration_function = [=](__unused Group *group, ObjectStore::Schema &schema) {
-        RLMRealmSetSchemaAndAlign(weakRealm, [RLMSchema.sharedSchema copy], schema);
+        RLMRealm *strongRealm = weakRealm;
+        if (!strongRealm) {
+            return;
+        }
+
+        RLMRealmSetSchemaAndAlign(strongRealm, [RLMSchema.sharedSchema copy], schema);
         RLMMigrationBlock userBlock = migrationBlockForPath(path);
-        if (userBlock && weakRealm) {
-            RLMMigration *migration = [[RLMMigration alloc] initWithRealm:weakRealm key:key error:nil];
+        if (userBlock) {
+            RLMMigration *migration = [[RLMMigration alloc] initWithRealm:strongRealm key:key error:nil];
             [migration execute:userBlock];
         }
     };
@@ -411,7 +416,6 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema, 
             // for readonly realms and dynamic realms without a custom schema just set the schema
             RLMSchema *targetSchema = readonly ? [RLMSchema.sharedSchema copy] : [RLMSchema dynamicSchemaFromRealm:realm];
             RLMRealmSetSchema(realm, targetSchema, true);
-            RLMRealmCreateAccessors(realm.schema);
         }
         else {
             // check cache for existing cached realms with the same path
@@ -435,8 +439,6 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema, 
                     RLMSetErrorOrThrow(RLMMakeError(RLMException(exception)), outError);
                     return nil;
                 }
-
-                RLMRealmCreateAccessors(realm.schema);
             }
 
             // initializing the schema started a read transaction, so end it
@@ -444,6 +446,7 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema, 
         }
 
         if (!dynamic) {
+            RLMRealmCreateAccessors(realm.schema);
             RLMCacheRealm(realm);
         }
     }
